18c66e7c1398a09e449e808fe52742d032ea0f2f
rohan20 <rohan.bvcoe@gmail.com>
2017-07-12 03:54:16 +0530

    Fixed text views

--- app/src/main/java/com/rohan/temp2/MainActivity.java
+++ app/src/main/java/com/rohan/temp2/MainActivity.java
@@ -22,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     TextView[] textViewsArray;
     EditText wordEditText;
     Button submitWordButton;
+    Button clearButton;
+    Button addLetterButton;
+
+    int j = -1;
 
     @Override
 
@@ -31,29 +35,54 @@ public class MainActivity extends AppCompatActivity {
 
         wordEditText = (EditText) findViewById(R.id.word_edit_text);
         submitWordButton = (Button) findViewById(R.id.submit_button);
+        clearButton = (Button) findViewById(R.id.clear_button);
+        addLetterButton = (Button) findViewById(R.id.add_letter_button);
+        textViewsLinearLayout = (LinearLayout) findViewById(R.id.text_views_linear_layout);
 
         submitWordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textViewsLinearLayout = (LinearLayout) findViewById(R.id.text_views_linear_layout);
-                textViewsLinearLayout.removeAllViews();
                 textViewsArray = new TextView[wordEditText.getText().toString().length()];
+
+                j = 0;
 
                 for (int i = 0; i < textViewsArray.length; i++) {
                     TextView textView = new TextView(getApplicationContext());
                     textView.setTextSize(20f);
-                    textView.setPadding(5,5,5,5);
+                    textView.setPadding(5, 5, 5, 5);
                     textView.setGravity(Gravity.CENTER);
                     textView.setTextColor(Color.BLACK);
-                    textView.setText(String.valueOf(i + 1));
-                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout
-                            .LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
+
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0,
+                            LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
                     textView.setLayoutParams(params);
+
                     textView.setBackgroundColor(Color.WHITE);
 
                     textViewsArray[i] = textView;
                     textViewsLinearLayout.addView(textView);
                 }
+            }
+        });
+
+        clearButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                j = -1;
+                textViewsLinearLayout.removeAllViews();
+            }
+        });
+
+        addLetterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(j == textViewsArray.length){
+                    return;
+                }
+
+                textViewsArray[j].setText(wordEditText.getText().toString());
+                j++;
             }
         });
 
